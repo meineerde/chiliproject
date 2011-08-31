1e9cfc93a360d433ff1335e4e1f03fbc6752ce21
Holger Just <h.just@finn.de>
2011-08-31 23:14:58 +0200

    Replace template_path with template_name

--- lib/chili_project/liquid/file_system.rb
+++ lib/chili_project/liquid/file_system.rb
@@ -1,13 +1,13 @@
 module ChiliProject
   module Liquid
     class FileSystem
-      def read_template_file(template_path, context)
-        raise ::Liquid::FileSystemError.new("Page not found") if template_path.blank?
+      def read_template_file(template_name, context)
+        raise ::Liquid::FileSystemError.new("Page not found") if template_name.blank?
         project = context['project'].object if context['project'].present?
 
-        cross_project_page = template_path.include?(':')
-        page = Wiki.find_page(template_path.to_s.strip, :project => (cross_project_page ? nil : project))
-        raise ::Liquid::FileSystemError.new("So such page '#{template_path}'") if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
+        cross_project_page = template_name.include?(':')
+        page = Wiki.find_page(template_name.to_s.strip, :project => (cross_project_page ? nil : project))
+        raise ::Liquid::FileSystemError.new("So such page '#{template_name}'") if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
 
         # Call textilizable on the view so all of the helpers are loaded
         # based on the view and not this tag
