fa6a62811377a90475fb4aaa59e1f85f1905853a
Holger Just <h.just@finn.de>
2011-08-31 22:57:15 +0200

    Move configuration to more appropriate places

--- lib/chili_project/liquid.rb
+++ lib/chili_project/liquid.rb
@@ -1,3 +1,8 @@
+require 'chili_project/liquid/variables'
+require 'chili_project/liquid/filters'
+require 'chili_project/liquid/tags'
+require 'chili_project/liquid/legacy'
+
 module ChiliProject
   module Liquid
   end
