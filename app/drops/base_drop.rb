2b2117bc27345220e2ade96000bf6eee4ec11255
Holger Just <h.just@finn.de>
2011-09-09 23:27:57 +0200

    Force permission checks on Liquid drops.
    Only explicitly allowed methods are allowed to be called by the liquid
    frontend.

--- app/drops/base_drop.rb
+++ app/drops/base_drop.rb
@@ -2,6 +2,7 @@ class BaseDrop < Liquid::Drop
   def initialize(object)
     @object = object unless object.respond_to?(:visible?) && !object.visible?
   end
+  attr_reader :object
 
   # Defines a Liquid method on the drop that is allowed to call the
   # Ruby method directly. Best used for attributes.
@@ -13,12 +14,27 @@ class BaseDrop < Liquid::Drop
         define_method sym do
           @object.send(:try, sym)
         end
+        liquid_keys << sym
       end
     end
   end
 
-protected
-  def object
-    @object
+  def invoke_drop(method_or_key)
+    # make sure we only allow explicitly allowed methods, don't provide
+    # default or internal methods to the frontend liquid.
+    if self.class.liquid_keys.include? method_or_key.to_s.to_sym
+      super
+    else
+      before_method(method_or_key)
+    end
+  end
+  alias :[] :invoke_drop
+
+  def self.liquid_keys
+    @liquid_keys ||= [:liquid_keys]
+  end
+
+  def liquid_keys
+    self.class.liquid_keys.collect(&:to_s)
   end
 end
