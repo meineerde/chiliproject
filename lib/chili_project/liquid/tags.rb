0b2e29141a17edf2b71049d31ddcdaa229445659
Holger Just <h.just@finn.de>
2011-10-16 18:17:16 +0200

    Pull BaseTag into its own file

--- lib/chili_project/liquid/tags.rb
+++ lib/chili_project/liquid/tags.rb
@@ -1,18 +1,5 @@
 module ChiliProject::Liquid
   module Tags
-    class BaseTag < ::Liquid::Tag
-      # @param args [Array, String] An array of strings in "key=value" format
-      # @param keys [Hash, Symbol] List of keyword args to extract
-      def extract_options(args, *keys)
-        options = {}
-        args.each do |arg|
-          if arg.to_s.gsub(/["']/,'').strip =~ %r{^(.+)\=(.+)$} && keys.include?($1.downcase.to_sym)
-            options[$1.downcase.to_sym] = $2
-          end
-        end
-        return options
-      end
-    end
 
     def self.register_tag(name, klass, options={})
       if options[:html]
