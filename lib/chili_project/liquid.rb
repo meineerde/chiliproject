1118f437d118cf7103a96e6b2508fbb5139c4421
Holger Just <h.just@finn.de>
2011-10-17 18:49:55 +0200

    Refactoring: Cleanup module definition

--- lib/chili_project/liquid.rb
+++ lib/chili_project/liquid.rb
@@ -1,12 +1,11 @@
+require 'chili_project/liquid/liquid_ext'
 require 'chili_project/liquid/variables'
 require 'chili_project/liquid/filters'
-require 'chili_project/liquid/liquid_ext'
 require 'chili_project/liquid/tags'
 require 'chili_project/liquid/legacy'
 
 module ChiliProject
   module Liquid
+    Liquid::Template.file_system = FileSystem.new
   end
 end
-
-Liquid::Template.file_system = ChiliProject::Liquid::FileSystem.new
