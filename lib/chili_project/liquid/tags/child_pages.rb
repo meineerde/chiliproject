4f75f45e2a94370d4a21acf3dbdf85ad9ef245d0
Holger Just <h.just@finn.de>
2011-10-17 18:58:49 +0200

    Don't return strings (or even error objects) but raise the actual exception instead

--- lib/chili_project/liquid/tags/child_pages.rb
+++ lib/chili_project/liquid/tags/child_pages.rb
@@ -1,7 +1,6 @@
 module ChiliProject::Liquid::Tags
   class ChildPages < BaseTag
     def initialize(tag_name, markup, tokens)
-      super
       if markup.present?
         tag_args = markup.split(',')
         @page_name = tag_args.shift.gsub(/["']/,'') # strip quotes
@@ -10,6 +9,7 @@ module ChiliProject::Liquid::Tags
         @options = extract_options(tag_args, :parent)
         @options[:parent] ||= false
       end
+      super
     end
 
     def render(context)
@@ -22,7 +22,7 @@ module ChiliProject::Liquid::Tags
       elsif @project.present?
         return render_all_pages(context)
       else
-        return TagError.new('child_pages', 'With no argument, this tag can be called from projects only.').to_s
+        raise TagError.new('With no argument, this tag can be called from projects only.')
       end
 
     end
@@ -33,17 +33,17 @@ module ChiliProject::Liquid::Tags
       cross_project_page = @page_name.include?(':')
       page = Wiki.find_page(@page_name.to_s, :project => (cross_project_page ? nil : @project))
 
-      return TagError.new('child_pages', 'Page not found').to_s if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
+      raise TagError.new("No such page '#{@page_name.to_s}'") if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
 
       pages = ([page] + page.descendants).group_by(&:parent_id)
-      return context.registers[:view].render_page_hierarchy(pages, @options[:parent] ? page.parent_id : page.id)
+      context.registers[:view].render_page_hierarchy(pages, @options[:parent] ? page.parent_id : page.id)
     end
 
     def render_all_pages(context)
       return '' unless @project.wiki.present? && @project.wiki.pages.present?
-      return TagError.new('child_pages', 'Page not found').to_s if !User.current.allowed_to?(:view_wiki_pages, @project)
+      raise TagError.new('child_pages', 'Page not found') if !User.current.allowed_to?(:view_wiki_pages, @project)
 
-      return context.registers[:view].render_page_hierarchy(@project.wiki.pages.group_by(&:parent_id))
+      context.registers[:view].render_page_hierarchy(@project.wiki.pages.group_by(&:parent_id))
     end
   end
 end
