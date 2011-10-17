731bf68d8b6da4779328fd72224cae7cf6b5c021
Holger Just <h.just@finn.de>
2011-10-17 18:16:59 +0200

    Fix typo

--- lib/chili_project/liquid/file_system.rb
+++ lib/chili_project/liquid/file_system.rb
@@ -7,7 +7,7 @@ module ChiliProject
 
         cross_project_page = template_name.include?(':')
         page = Wiki.find_page(template_name.to_s.strip, :project => (cross_project_page ? nil : project))
-        raise ::Liquid::FileSystemError.new("So such page '#{template_name}'") if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
+        raise ::Liquid::FileSystemError.new("No such page '#{template_name}'") if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
 
         page.content
       end
