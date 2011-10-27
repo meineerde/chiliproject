6b693bbdd7c451d424876f0600f70b9aef64beca
Holger Just <h.just@finn.de>
2011-10-27 18:23:34 +0200

    Check for circular inclusion

--- lib/chili_project/liquid/tags/include.rb
+++ lib/chili_project/liquid/tags/include.rb
@@ -38,16 +38,34 @@ module ChiliProject::Liquid::Tags
     end
 
   private
+    def break_circle(context)
+      context.registers[:included_pages] ||= []
+
+      project = context['project'].identifier if context['project'].present?
+      template_name = context[@template_name]
+      cross_project_page = template_name.include?(':')
+      page_title = cross_project_page ? template_name : "#{project}:#{template_name}"
+
+      raise ::Liquid::FileSystemError.new("Circular inclusion detected") if context.registers[:included_pages].include?(page_title)
+      context.registers[:included_pages] << page_title
+
+      yield
+    ensure
+      context.registers[:included_pages].pop
+    end
+
     def _template_source(wiki_content)
       wiki_content.text
     end
 
     def _render_partial(partial, template, context)
-      textile = partial.render(context)
+      break_circle(context) do
+        textile = partial.render(context)
 
-      # Call textilizable on the view so all of the helpers are loaded
-      # based on the view and not this tag
-      context.registers[:view].textilizable(textile, :attachments => template.page.attachments, :headings => false)
+        # Call textilizable on the view so all of the helpers are loaded
+        # based on the view and not this tag
+        context.registers[:view].textilizable(textile, :attachments => template.page.attachments, :headings => false)
+      end
     end
 
     def _read_template_from_file_system(context)
