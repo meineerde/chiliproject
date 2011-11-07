a5836cee4596ad36571f8e86752bc3d7a3d5f6c5
Holger Just <h.just@finn.de>
2011-11-07 15:47:18 +0100

    Make the child_pages tag backwards compatible.
    The tag should be replaced with a more flexible syntax like
    {{ page.children.list }}

--- lib/chili_project/liquid/tags/base_tag.rb
+++ lib/chili_project/liquid/tags/base_tag.rb
@@ -2,14 +2,15 @@ module ChiliProject::Liquid::Tags
   class BaseTag < ::Liquid::Tag
     # @param args [Array, String] An array of strings in "key=value" format
     # @param keys [Hash, Symbol] List of keyword args to extract
-    def extract_options(args, *keys)
+    def extract_macro_options(args, *keys)
       options = {}
       args.each do |arg|
         if arg.to_s.gsub(/["']/,'').strip =~ %r{^(.+)\=(.+)$} && keys.include?($1.downcase.to_sym)
           options[$1.downcase.to_sym] = $2
+          args.pop
         end
       end
-      return options
+      return [args, options]
     end
   end
 end
--- lib/chili_project/liquid/tags/child_pages.rb
+++ lib/chili_project/liquid/tags/child_pages.rb
@@ -1,47 +1,45 @@
 module ChiliProject::Liquid::Tags
   class ChildPages < BaseTag
     def initialize(tag_name, markup, tokens)
+      markup = markup.strip.gsub(/["']/, '')
       if markup.present?
         tag_args = markup.split(',')
-        @page_name = tag_args.shift.gsub(/["']/,'') # strip quotes
-        @page_name.strip!
-
-        @options = extract_options(tag_args, :parent)
-        @options[:parent] ||= false
+        @args, @options = extract_macro_options(tag_args, :parent)
+      else
+        @args = []
+        @options = {}
       end
       super
     end
 
     def render(context)
       # inside of a project
-      @project = context['project'].object if context['project'].present?
+      @project = Project.find(context['project'].identifier) if context['project'].present?
 
-      if @page_name.present? &&
-          (@project.present? || @page_name.include?(':')) # Allow cross project use with project:page_name
-        return render_child_pages_from_single_page(context)
-      elsif @project.present?
+      if @args.present?
+        page_name = @args.first.to_s
+        cross_project_page = page_name.include?(':')
+
+        page = Wiki.find_page(page_name, :project => (cross_project_page ? nil : @project))
+      # FIXME: :object and :attribute should be variables, not registers
+      elsif context.registers[:object].is_a?(WikiContent)
+        page = context.registers[:object].page
+        page_name = page.title
+      elsif @project
         return render_all_pages(context)
       else
         raise TagError.new('With no argument, this tag can be called from projects only.')
       end
 
-    end
-
-  private
-
-    def render_child_pages_from_single_page(context)
-      cross_project_page = @page_name.include?(':')
-      page = Wiki.find_page(@page_name.to_s, :project => (cross_project_page ? nil : @project))
-
-      raise TagError.new("No such page '#{@page_name.to_s}'") if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
-
+      raise TagError.new("No such page '#{page_name}'") if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
       pages = ([page] + page.descendants).group_by(&:parent_id)
       context.registers[:view].render_page_hierarchy(pages, @options[:parent] ? page.parent_id : page.id)
     end
 
+  private
     def render_all_pages(context)
       return '' unless @project.wiki.present? && @project.wiki.pages.present?
-      raise TagError.new('child_pages', 'Page not found') if !User.current.allowed_to?(:view_wiki_pages, @project)
+      raise TagError.new('Page not found') if !User.current.allowed_to?(:view_wiki_pages, @project)
 
       context.registers[:view].render_page_hierarchy(@project.wiki.pages.group_by(&:parent_id))
     end
